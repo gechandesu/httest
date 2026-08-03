637a5d21e660dac7e5903227a4fae6989a4427da
ge <gechandev@gmail.com>
2026-08-03 21:58:11 +0300

    update help text

--- prefs.v
+++ prefs.v
@@ -29,7 +29,8 @@ mut:
 	listen_addr       string   @[ignore]
 	ipv4_only         bool     @[long: 'ipv4'; xdoc: 'enable IPv4-only mode.']
 	ipv6_only         bool     @[long: 'ipv6'; xdoc: 'enable IPv6-only mode.']
-	backlog           int    = 128      @[xdoc: 'max number of parallel connections on socket, defaults to 128.']
+	backlog           int = 128      @[xdoc: 'max number of parallel connections on socket, defaults to 128.']
+	http2_0           bool     @[long: 'http2.0'; xdoc: 'use HTTP/2.0 instead of HTTP/1.1.']
 	log_level         string = 'info'   @[xdoc: 'log level, one of: none, fatal, error, warn, info, debug, trace.']
 	log_output        string = 'stderr'   @[xdoc: 'where to write logs: stdout, stderr (default) or filepath.']
 	log_fields        string   @[short: 'F'; xdoc: 'See Log fields control below.']
@@ -39,39 +40,37 @@ mut:
 	respond_body      string   @[xdoc: 'response body as string.']
 	respond_file      string   @[xdoc: 'read response body from file.']
 	response_delay    string   @[xdoc: 'response delay e.g 1s, 3m, 100-900ms, 300 (in milliseconds by default).']
-	cgi_script        string   @[xdoc: 'path to CGI script to execute for each request.']
+	cgi_script        string   @[xdoc: 'path to CGI script to execute for each request. -respond* options is ignored.']
 }
 
 const help_header = 'Usage: ${name} [OPTION]... [ADDR]
 
-ADDR may be an IPv4 or IPv6 IP-address, domain name or UNIX-socket path.'
+ADDR may be an IPv4 or IPv6 address, :PORT, domain name or UNIX socket path.'
 
 const help_footer = '
-When -cgi-script is set, the script handles the request instead of the static
--respond* options.
-
 Log fields control:
+  Below are listed all available log fields in the order they appear in the
+  logs. Fields marked by * are non-defaults.
 
-Below are listed all available log fields in the order they appear in the logs.
-Fields marked by * are non-defaults.
+    id          auto-generated or passed through -request-id-header request ID.
+    method      used HTTP method.
+    path        request path.
+    status      HTTP response status code as integer.
+    recv        size of request body in bytes.
+    sent        size of response body in bytes.
+    elapsed     request processing time excluding HTTP parsing time.
+    remote*     remote address from Remote-Addr header.
+    user_agent* request User-Agent header value.
+    headers*    all request headers separated by `;`.
+    body*       request body text as is.
 
-  id          auto-generated or passed through -request-id-header request ID.
-  method      used HTTP method.
-  path        request path.
-  status      HTTP response status code as integer.
-  recv        size of request body in bytes.
-  sent        size of response body in bytes.
-  elapsed     request processing time excluding HTTP parsing time.
-  remote*     remote address from Remote-Addr header.
-  user_agent* request User-Agent header value.
-  headers*    all request headers separated by `;`.
-  body*       request body text as is.
+  You can add or remove fields from the HTTP request log using the -log-fields
+  (-F) option. To add a field, specify its name without a prefix or with a `+`
+  prefix. For example `-F +body` enables request body logging. You can list
+  multiple fields separated by commas. `-` prefix disables the log field.
+  There is also two special values `all` and `default` (for default set).
+  Examples:
 
-You can add or remove fields from the HTTP request log using the -log-fields
-(-F) option. To add a field, specify its name without a prefix or with a `+`
-prefix. For example `-F +body` enables request body logging. You can list
-multiple fields separated by commas. `-` prefix disables the log field. There
-is also two special values `all` and `default` (for default set). Examples:
-  httest -F +headers,body,-id
-  httest -F +all
-  httest -F -default,+id,method,path,status,elapsed'
+    httest -F +headers,body,-id
+    httest -F +all
+    httest -F -default,+id,method,path,status,elapsed'
